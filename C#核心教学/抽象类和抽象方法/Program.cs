d0afe5e2a0d9d90a8237c748a714d1e6fb9c2485
moyujie <loli1331206@163.com>
2024-11-25 23:33:36 +0800

    2

---
+++ C#核心教学/抽象类和抽象方法/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace 抽象类和抽象方法
+{
+    #region 知识点一 抽象类
+    //概念
+    //被抽象关键字abstract修饰的类
+    //特点：
+    //1.不能被实例化的类
+    //2.可以包含抽象方法
+    //3.继承抽象类必须重写其中抽象方法
+
+    abstract class Thing
+    {
+        //抽象类中 封装的所有知识点都可以在其中书写
+        public string name;
+        
+        //可以在抽象类中写抽象函数
+    }
+
+    class Water:Thing
+    {
+        
+    }
+    #endregion
+
+    #region 知识点二 抽象函数
+    //又叫 纯虚方法
+    //用abstract关键字修饰的方法
+    //特点：
+    //1.只能在抽象类中申明
+    //2.没有方法体
+    //3.不能是私有的
+    //4.继承后必须实现 用override重写
+
+    abstract class Fruits
+    {
+        public string name;
+        //抽象方法 是一定不能有函数体的
+        public abstract void ruin();
+
+        public virtual void Test()
+        {
+            //可以选择是否写逻辑
+        }
+    }
+
+    class Apple:Fruits
+    {
+        public override void ruin()
+        {
+            throw new NotImplementedException();
+        }
+        //虚方法是可以由子类选择性来实现的
+        //抽象方法必须实现
+    }
+
+    class SuperApple:Apple
+    {
+        //虚方法和抽象方法 都可以被子类无限地 去重写
+        public override void ruin()
+        {
+            base.ruin();
+        }
+
+        public override void Test()
+        {
+            base.Test();
+        }
+    }
+    #endregion
+    class  Program
+    {
+        static void Main(string[] args)
+        {
+            //抽象类不能被实例化
+            //但是可以遵循里氏替换原则 用父类容器装子类
+            Thing t = new Water();
+        }
+    }
+    //总结
+    //抽象类 被abstract修饰的类 不能被实例化 可以包含抽象方法
+    //抽象方法 没有方法体的纯虚方法 继承后必须去实现的方法
+    //注意：
+    //如何选择普通类还是抽象类
+    //不希望被实例化的对象 相对比较抽象的类可以使用抽象类
+    //父类中的行为不太需要被实现的，只希望子类去定义具体的规则的 可以选择 抽象类然后去使用其中的抽象方法来定义规则
+    //作用
+    //整体框架设计时会使用
+}
---
+++ C#核心教学/继承/密封类/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace 密封类
+{
+    #region 知识点一 基本概念
+    //密封类 是使用 sealed密封关键字修饰的类
+    //作用：让类无法再被继承
+
+    #endregion
+
+    #region 知识点二 实例
+
+    class Father
+    {
+        
+    }
+
+    sealed class Son : Father
+    {
+        
+    }
+    
+    #endregion
+
+    #region 知识点三 作用
+    //在面向对象程序的设计中，密封类的主要作用就是不允许最底层子类被继承
+    //可以保证程序的规范性，安全性
+    //目前对于大家来说 可能用处不大
+    //以后制作复杂系统或者程序框架时 能慢慢体会密封的作用
+    #endregion
+    //总结
+    //关键字：sealed
+    //作用：让类无法再被继承
+    //意义：加强面向对象程序设计的 规范性 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
